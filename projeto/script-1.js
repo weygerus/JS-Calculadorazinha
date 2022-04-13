af7abcad9b3e7e9c63716f2f73ec66100e78438f
weygerus <gabrileao38@gmail.com>
2022-04-13 14:55:30 -0300

    13/04/2022-1

---
+++ projeto/script-1.js
@@ -0,0 +1,25 @@
+function inserir(caracter) {    //coloca o valor de cada botão selecionado em uma string
+    document.form.visor.value = document.form.visor.value + caracter;
+}
+function igual(){
+    expressao = document.form.visor.value;
+    
+    if(expressao)
+    {
+        document.form.visor.value = eval(expressao); //eval() -- avalia a expressão aritmetica da string
+        document.form.visorconta.value = expressao + '=';
+    }else{
+        document.getElementById('somar').ariaDisabled;
+    }
+}
+function limpar(){
+    document.form.visor.value = "";
+    document.form.visorconta.value = "";
+}
+function voltar(){
+    var expressao = document.form.visor.value;
+    document.form.visor.value = expressao.substring(0, expressao.length-1);
+}
+
+
+
